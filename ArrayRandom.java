74451cc08390271fe9b75b9bbb1e2cd2e984cf53
KDpogz <bernz19XD@gmail.com>
2016-09-07 18:01:52 +0800

    Add files via upload
    Finish na to!!!

---
+++ ArrayRandom.java
@@ -0,0 +1,78 @@
+package jrandom;
+import java.util.*;
+
+public class ArrayRandom {
+		int sum=0,num;
+		int max=0;
+		double average;
+		int how;
+		int  array [];
+		
+		public void getAve(){
+			
+		
+				average=(sum/array.length);
+				
+				System.out.println("Average :" +average);
+		
+		}
+		
+		public void getSum(){
+			for (int i = 0; i<=how-1;i++){
+				sum = sum+array[i];			
+			}
+			System.out.println("Sum :" +sum);
+
+		}
+
+		public void getMax(){
+
+			
+			for (int m=0; m<=how-1; m++ ){
+				if (array[m]>max){
+					max=array[m];
+				}		
+			}
+			System.out.println("Max:" +max);
+		}
+		
+		public void getInput(){
+			
+			Scanner input = new Scanner (System.in);
+				Random rad = new Random();
+			
+		 System.out.println("How many Numbers: ");
+			
+			how= input.nextInt();	
+			array= new int[how];
+			
+			for(int r=0; r<=how-1;r++){
+			
+				  array[r]=rad.nextInt(1000);
+			  }
+			
+			for (int p=0;p <=how-1; p++){
+				System.out.print(array[p]+"  ");
+				
+			}
+			
+			System.out.println();
+		
+		 
+		}
+		
+		
+		public static void main(String[]args){
+			ArrayRandom arrayobject = new ArrayRandom();
+				
+				arrayobject.getInput();
+				arrayobject.getSum();
+				arrayobject.getAve();
+				arrayobject.getMax();
+		}
+
+	}
+
+
+
+
